30a7911ff1d7d1063b9f49700772359cfca49a7d
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2023-04-25 17:30:25 +0530

    Code addition in a new repo

---
+++ src/components/ProfileDropdown.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
+
+const ProfileDropdown = () => {
+    const { user, getUser, logout, updateProfilePicture } = useContext(AuthContext);
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleUpdateProfilePicture = async () => {
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append('profilePicture', selectedFile);
+
+            await updateProfilePicture(formData);
+        } else {
+            // Show an error message if no file is selected
+            console.error('Please select a file to update your profile picture.');
+        }
+    };
+
+    useEffect(() => {
+        const handleUser = async (e) => {
+            if (e) e.preventDefault();
+
+            getUser();
+        };
+
+        handleUser();
+    }, [getUser]);
+
+    return (
+        <div className="profile-dropdown">
+            <img
+                src={`/backend/${user.profilePicture}` || "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"}
+                alt="Profile"
+                className="profile-picture"
+            />
+
+            <div className="dropdown-content">
+                <div className="user-details">
+                    <p><strong>{user.username}</strong></p>
+                    <p>{user.email}</p>
+                </div>
+                <form>
+                    <div className="form-group">
+                        <input
+                            type="file"
+                            className="form-control-file"
+                            onChange={handleFileChange}
+                        />
+                    </div>
+                    <button onClick={handleUpdateProfilePicture} type="button" className="btn btn-primary">
+                        Update Profile Picture
+                    </button>
+                </form>
+                <Link className='nav-link' onClick={logout}>Logout</Link>
+            </div>
+        </div>
+    )
+};
+
+export default ProfileDropdown;
